e74a0ba77ec0ec168f6ac1cb0f7c34af6aa0717e
Balaji Bathini <rishibathini18@gmail.com>
2026-07-17 14:41:27 +0530

    circular dependency issue

--- hr_employee_custom/__manifest__.py
+++ hr_employee_custom/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Human Resources',
     'summary': 'Customizations for Employee Master',
     'author': 'Balaji Bathini',
-    'depends': ['hr', 'hr_payroll', 'hr_employee_updation', 'hr_employee_entended', 'EHS', 'l10n_in_hr_payroll','custom_css_ui'],
+    'depends': ['hr', 'hr_payroll', 'hr_employee_updation', 'hr_employee_entended', 'EHS', 'custom_css_ui'],
     'data': [
         'views/hr_employee_views.xml',
     ],
